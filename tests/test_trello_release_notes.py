b294d3418c32afb82ad7db8d02e6e0b6106722ab
Matt Katz <matthew.katz+git@morelightmorelight.com>
2019-10-04 11:37:59 -0400

    black format the file

--- tests/test_trello_release_notes.py
+++ tests/test_trello_release_notes.py
@@ -11,14 +11,21 @@ from trello_release_notes.__main__ import get_arg_parser
 from collections import namedtuple
 from pathlib import Path
 
-test_ini = Path( "tests/trello_test_settings.ini")
+test_ini = Path("tests/trello_test_settings.ini")
 args = None
-if  test_ini.exists():
+if test_ini.exists():
     args = get_arg_parser().parse_args(["--config", str(test_ini)])
 else:
     Args = namedtuple("Args", "apikey apisecret boardname done_list releases")
     from os import environ
-    args = Args(environ['apikey'], environ['apisecret'], environ['boardname'], environ['done_list'], environ['releases'])
+
+    args = Args(
+        environ["apikey"],
+        environ["apisecret"],
+        environ["boardname"],
+        environ["done_list"],
+        environ["releases"],
+    )
 
 boardname = args.boardname
 done_name = args.done_list
