8783bc5d88295c40108f3b967cfc8c6bf3967f77
Matt Katz <matthew.katz+git@morelightmorelight.com>
2019-12-01 15:27:09 -0500

    Bump version: 0.3.0 → 0.3.1

--- setup.py
+++ setup.py
@@ -60,6 +60,6 @@ setup(
     test_suite='tests',
     tests_require=EXTRAS_REQUIRE["tests"],
     url='https://github.com/mattkatz/trello_release_notes',
-    version='0.3.0',
+    version='0.3.1',
     zip_safe=False,
 )
--- trello_release_notes/__init__.py
+++ trello_release_notes/__init__.py
@@ -4,4 +4,4 @@
 
 __author__ = """Matt Katz"""
 __email__ = "github@morelightmorelight.com"
-__version__ = "__version__ = '0.3.0'"
+__version__ = "__version__ = '0.3.1'"
